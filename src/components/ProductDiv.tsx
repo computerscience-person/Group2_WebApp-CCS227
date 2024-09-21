5b748d0faecaf968168a919ade8016c725e288d3
Cristopher-Artacho-WVSU <cristopherian.artacho@wvsu.edu.ph>
2024-09-21 09:21:46 +0800

    Added images in product, chenged width from px to percentage

--- src/components/ProductDiv.tsx
+++ src/components/ProductDiv.tsx
@@ -20,14 +20,14 @@ const ProductDiv: React.FC<ProductDivProps> = ({
       className="w-[300px] h-[420px] relative bg-blue-200 rounded-lg" // Change to relative if you don't need flex
       style={{
         position: "absolute",
-        left: `${placingLeft}px`,
+        left: `${placingLeft}%`,
         top: `${placingTop}px`,
       }}
     >
       <img
         src={imageSrc}
         alt="Product"
-        className="w-[180px] h-[300px] object-cover mb-8 items-center"
+        className="w-[100%] h-[300px] object-cover mb-8 items-center justify-center flex"
       />
       {/* Add text-left to ensure left alignment */}
       <p className="mt-4 text-lg ml-4">{productName}</p>
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -2,85 +2,93 @@ import NavbarBlue from "../components/Navbars/NavbarBlue";
 import FooterBlue from "../components/Footers/FooterBlue";
 import ProductDiv from "../components/ProductDiv";
 
+import wu_classic from "../assets/products/waterup_classic.png";
+import wu_lemon from "../assets/products/waterup_lemon.png";
+import wu_strawberry from "../assets/products/waterup_strawberry.png";
+import wu_peach from "../assets/products/waterup_peach.png";
+import wu_assorted from "../assets/products/waterup_intro.png";
+import wu_oneFlavor4Pack from "../assets/products/waterup_four.png";
+
 const ProductPage = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <NavbarBlue />
-      {/* <main className="flex-grow flex items-center justify-center p-4">
-        <h1 className="text-center text-2xl font-bold">
-          <strong>Hello World</strong>
-        </h1>
-      </main> */}
+
       {/* LAYER 1 BACKGROUND */}
-      <div className="w-[1519px] h-[648px]">
+      <div className="w-[100%] h-[648px] bg-[url('./assets/background.png')]">
         {/* TAGLINE */}
-        <div className="absolute w-[832px] h-[170px] left-[74px] top-[190px] font-poppins font-bold text-[72px] leading-[108px]">
+        <div className="absolute w-[832px] h-[170px] left-[80px] top-[190px] font-poppins font-bold text-[72px] leading-[108px]">
           Water intake? Not a problem.
         </div>
         {/* DESCRIPTION */}
-        <div className="absolute w-[760px] h-[100px] left-[78px] top-[500px] font-poppins font-normal text-[30px]">
+        <div className="absolute w-[760px] h-[100px] left-[80px] top-[500px] font-poppins font-normal text-[30px]">
           Thirsty, but tired of drinking plain water? Don’t worry, Water-up has
           your back.
         </div>
+        <img
+          src={wu_classic}
+          className="absolute left-[900px] top-[100px] w-auto h-[600px] mr-[80px] transition-transform duration-700 ease-in-out hover:scale-105 "
+          alt="Water-Up Classic"
+        />
       </div>
 
       {/* LAYER 2 BACKGROUND */}
-      <div className="w-[1519px] h-[1500px]">
+      <div className="w-[100%] h-[1500px] mb-8">
         {/* OUR PRODUCTS */}
-        <div className="absolute w-[484px] h-[108px] left-[174px] top-[700px] font-poppins font-normal text-[72px] leading-[108px]">
+        <div className=" absolute w-[484px] h-[108px] left-[15%] top-[750px] font-poppins font-normal text-[72px] leading-[108px]">
           <strong>Our Products</strong>
         </div>
         {/* DESCRIPTION */}
-        <div className="absolute w-[1300px] h-[106px] left-[174px] top-[840px] font-poppins font-normal text-[30px] leading-[52px]">
+        <div className=" absolute w-[1300px] h-[106px] left-[15%] top-[890px] font-poppins font-normal text-[30px] leading-[52px]">
           No matter what they say, no matter what they do, we gonna re-hydrate.
           Keep watering the flowers until it blooms.
         </div>
 
         {/* PRODUCT 1*/}
         <ProductDiv
-          placingLeft={150}
-          placingTop={1100}
-          imageSrc="./assets/products/waterup_classic.png"
+          placingLeft={7}
+          placingTop={1150}
+          imageSrc={wu_classic}
           productName="Product Name"
           productPrice="₱00.00"
         />
         {/* PRODUCT 2*/}
         <ProductDiv
-          placingLeft={600}
-          placingTop={1100}
-          imageSrc="./assets/products/waterup_classic.png"
+          placingLeft={41}
+          placingTop={1150}
+          imageSrc={wu_lemon}
           productName="Product Name"
           productPrice="₱00.00"
         />
         {/* PRODUCT 3*/}
         <ProductDiv
-          placingLeft={1050}
-          placingTop={1100}
-          imageSrc="./assets/products/waterup_classic.png"
+          placingLeft={75}
+          placingTop={1150}
+          imageSrc={wu_strawberry}
           productName="Product Name"
           productPrice="₱00.00"
         />
         {/* PRODUCT 4*/}
         <ProductDiv
-          placingLeft={150}
-          placingTop={1700}
-          imageSrc="./assets/products/waterup_classic.png"
+          placingLeft={7}
+          placingTop={1650}
+          imageSrc={wu_peach}
           productName="Product Name"
           productPrice="₱00.00"
         />
         {/* PRODUCT 5*/}
         <ProductDiv
-          placingLeft={600}
-          placingTop={1700}
-          imageSrc="./assets/products/waterup_classic.png"
+          placingLeft={41}
+          placingTop={1650}
+          imageSrc={wu_assorted}
           productName="Product Name"
           productPrice="₱00.00"
         />
         {/* PRODUCT 6*/}
         <ProductDiv
-          placingLeft={1050}
-          placingTop={1700}
-          imageSrc="./assets/products/waterup_classic.png"
+          placingLeft={75}
+          placingTop={1650}
+          imageSrc={wu_oneFlavor4Pack}
           productName="Product Name"
           productPrice="₱00.00"
         />
