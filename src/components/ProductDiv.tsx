28b98a7200c0b92c098eb331848608fcccc059ea
Cristopher-Artacho-WVSU <cristopherian.artacho@wvsu.edu.ph>
2024-09-22 16:29:36 +0800

    changed layout to gridding, adjusted ProductDiv parameters

--- src/components/ProductDiv.tsx
+++ src/components/ProductDiv.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 
 interface ProductDivProps {
-  placingLeft: number;
-  placingTop: number;
+  // placingLeft: number;
+  // placingTop: number;
   imageSrc: string;
   productName: string;
   productPrice: string;
 }
 
 const ProductDiv: React.FC<ProductDivProps> = ({
-  placingLeft,
-  placingTop,
+  // placingLeft,
+  // placingTop,
   imageSrc,
   productName,
   productPrice,
 }) => {
   return (
     <div
-      className="w-[300px] h-[420px] relative bg-blue-200 rounded-lg" // Change to relative if you don't need flex
-      style={{
-        position: "absolute",
-        left: `${placingLeft}%`,
-        top: `${placingTop}px`,
-      }}
+    // className="w-[300px] h-[344.90px] relative bg-blue-200 rounded-lg" // Change to relative if you don't need flex
+    // style={{
+    //   position: "absolute",
+    //   left: `${placingLeft}%`,
+    //   top: `${placingTop}px`,
+    // }}
     >
       <img
         src={imageSrc}
         alt="Product"
-        className="w-[100%] h-[300px] object-cover mb-8 items-center justify-center flex"
+        className="w-[65%] h-[294.32.px] object-cover mb-8 mx-auto mt-4"
       />
       {/* Add text-left to ensure left alignment */}
       <p className="mt-4 text-lg ml-4">{productName}</p>
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -13,86 +13,82 @@ const ProductPage = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <NavbarBlue />
-
-      {/* LAYER 1 BACKGROUND */}
-      <div className="w-[100%] h-[648px] bg-[url('./assets/background.png')]">
+      {/* LAYER 1 GRIDDING */}
+      <div className="grid grid-cols-12 grid-rows-5 gap-4 h-[648px] bg-[url('./assets/background.png')] text-[#FFFFFF]">
         {/* TAGLINE */}
-        <div className="absolute w-[832px] h-[170px] left-[80px] top-[190px] font-poppins font-bold text-[72px] leading-[108px]">
+        <div className="col-start-2 col-end-8 row-start-2 font-poppins font-bold text-[65px] leading-[108px] ">
           Water intake? Not a problem.
         </div>
         {/* DESCRIPTION */}
-        <div className="absolute w-[760px] h-[100px] left-[80px] top-[500px] font-poppins font-normal text-[30px]">
+        <div className="col-start-2 col-end-8 row-start-4 font-poppins font-normal text-[27px] ">
           Thirsty, but tired of drinking plain water? Don’t worry, Water-up has
           your back.
         </div>
+        {/* PRODUCT IMAGE */}
         <img
           src={wu_classic}
-          className="absolute left-[900px] top-[100px] w-auto h-[600px] mr-[80px] transition-transform duration-700 ease-in-out hover:scale-105 "
+          className=" col-start-8 col-end-12 row-span-5 w-auto h-[85%] transition-transform duration-700 ease-in-out hover:scale-105 mt-7 ml-10"
           alt="Water-Up Classic"
         />
       </div>
 
-      {/* LAYER 2 BACKGROUND */}
-      <div className="w-[100%] h-[1500px] mb-8">
+      {/* LAYER 2 GRIDDING */}
+      <div className="grid grid-cols-8 grid-rows-10 gap-4 h-[1500px] text-[#7181A7]">
         {/* OUR PRODUCTS */}
-        <div className=" absolute w-[484px] h-[108px] left-[15%] top-[750px] font-poppins font-normal text-[72px] leading-[108px]">
+        <div className=" col-start-2 col-end-5 row-start-2 font-poppins font-normal text-[72px] leading-[108px]">
           <strong>Our Products</strong>
         </div>
         {/* DESCRIPTION */}
-        <div className=" absolute w-[1300px] h-[106px] left-[15%] top-[890px] font-poppins font-normal text-[30px] leading-[52px]">
+        <div className=" col-start-2 col-end-8 row-start-3 font-poppins font-normal text-[27px] leading-[52px]">
           No matter what they say, no matter what they do, we gonna re-hydrate.
           Keep watering the flowers until it blooms.
         </div>
+        {/* PRODUCT */}
+        <div className=" col-start-2 col-end-4 row-start-4 row-end-7 bg-blue-200 m-8 rounded-[45px]">
+          <ProductDiv
+            imageSrc={wu_classic}
+            productName="Product Name"
+            productPrice="₱00.00"
+          />
+        </div>
+        <div className=" col-start-4 col-end-6 row-start-4 row-end-7 bg-blue-200 m-8 rounded-[45px]">
+          <ProductDiv
+            imageSrc={wu_lemon}
+            productName="Product Name"
+            productPrice="₱00.00"
+          />
+        </div>
+        <div className=" col-start-6 col-end-8 row-start-4 row-end-7 bg-blue-200 m-8 rounded-[45px]">
+          <ProductDiv
+            imageSrc={wu_strawberry}
+            productName="Product Name"
+            productPrice="₱00.00"
+          />
+        </div>
 
-        {/* PRODUCT 1*/}
-        <ProductDiv
-          placingLeft={7}
-          placingTop={1150}
-          imageSrc={wu_classic}
-          productName="Product Name"
-          productPrice="₱00.00"
-        />
-        {/* PRODUCT 2*/}
-        <ProductDiv
-          placingLeft={41}
-          placingTop={1150}
-          imageSrc={wu_lemon}
-          productName="Product Name"
-          productPrice="₱00.00"
-        />
-        {/* PRODUCT 3*/}
-        <ProductDiv
-          placingLeft={75}
-          placingTop={1150}
-          imageSrc={wu_strawberry}
-          productName="Product Name"
-          productPrice="₱00.00"
-        />
-        {/* PRODUCT 4*/}
-        <ProductDiv
-          placingLeft={7}
-          placingTop={1650}
-          imageSrc={wu_peach}
-          productName="Product Name"
-          productPrice="₱00.00"
-        />
-        {/* PRODUCT 5*/}
-        <ProductDiv
-          placingLeft={41}
-          placingTop={1650}
-          imageSrc={wu_assorted}
-          productName="Product Name"
-          productPrice="₱00.00"
-        />
-        {/* PRODUCT 6*/}
-        <ProductDiv
-          placingLeft={75}
-          placingTop={1650}
-          imageSrc={wu_oneFlavor4Pack}
-          productName="Product Name"
-          productPrice="₱00.00"
-        />
+        <div className=" col-start-2 col-end-4 row-start-7 row-end-10 bg-blue-200 m-8 rounded-[45px]">
+          <ProductDiv
+            imageSrc={wu_peach}
+            productName="Product Name"
+            productPrice="₱00.00"
+          />
+        </div>
+        <div className=" col-start-4 col-end-6 row-start-7 row-end-10 bg-blue-200 m-8 rounded-[45px]">
+          <ProductDiv
+            imageSrc={wu_assorted}
+            productName="Product Name"
+            productPrice="₱00.00"
+          />
+        </div>
+        <div className=" col-start-6 col-end-8 row-start-7 row-end-10 bg-blue-200 m-8 rounded-[45px]">
+          <ProductDiv
+            imageSrc={wu_oneFlavor4Pack}
+            productName="Product Name"
+            productPrice="₱00.00"
+          />
+        </div>
       </div>
+
       <FooterBlue />
     </div>
   );
