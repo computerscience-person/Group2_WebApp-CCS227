d5ea43aa4b4fed3ed7d8d9e421978928a0dec24d
Oliver Ladores <oliver.ladores@wvsu.edu.ph>
2024-09-24 13:14:32 +0800

    Did some code cleanup. Getting ready for release.
    - Fixed linter errors.
    - Ran through formatter.

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -18,22 +18,24 @@ const settings = {
 };
 
 function Carousel() {
-    return (
-        <div className="w-3/4 m-auto mb-[100px] mt-20">
-            <div className="flex flex-row justify-center gap-10">
-            <h1 className="font-poppins font-bold text-6xl mt-10"
-            style={{ color: "#7181A7" }}>
-                Meet the people behind
-            </h1>
-            <img src= {wu_blue} alt="" className="h-40 w-50"/> 
-            </div>
-            <div className="mt-20">
-            <Slider {...settings}>
-                {data.map((d) => (
-                    <div className="h-[400px] rounded-xl">
-                        <div className="h-56 rounded-t-xl flex justify-center items-center">
-                            <img src ={d.img} alt="" className="h-80 w-80" />
-                        </div>
+  return (
+    <div className="w-3/4 m-auto mb-[100px] mt-20">
+      <div className="flex flex-row justify-center gap-10">
+        <h1
+          className="font-poppins font-bold text-6xl mt-10"
+          style={{ color: "#7181A7" }}
+        >
+          Meet the people behind
+        </h1>
+        <img src={wu_blue} alt="" className="h-40 w-50" />
+      </div>
+      <div className="mt-20">
+        <Slider {...settings}>
+          {data.map((d) => (
+            <div className="h-[400px] rounded-xl" key={d.name}>
+              <div className="h-56 rounded-t-xl flex justify-center items-center">
+                <img src={d.img} alt="" className="h-80 w-80" />
+              </div>
 
               <div className="flex flex-col justify-center items-center gap-4 p-4">
                 <h2
@@ -55,40 +57,36 @@ function Carousel() {
 }
 
 const data = [
-    {
-        name: "Cristopher Ian Artarcho",
-        img: cris,
-        title: "Founder"
-    },
-    {
-        name: "John Manuel Carado",
-        img: mans,
-        title: "Founder"
-    },
-    {
-        name: "Angelika Marie Nava",
-        img: aika,
-        title: "Founder"
-    },
-    {
-        name: "Reycel Sarmiento",
-        img: reycel,
-        title: "Founder"
-    },
-    {
-        name: "Els Dave Constantino",
-        img: els,
-        title: "Founder"
-    },
-    {
-        name: "Oliver Ladores",
-        img: ollie,
-        title: "Founder"
-    }
-
-
-]
-
-
+  {
+    name: "Cristopher Ian Artarcho",
+    img: cris,
+    title: "Founder",
+  },
+  {
+    name: "John Manuel Carado",
+    img: mans,
+    title: "Founder",
+  },
+  {
+    name: "Angelika Marie Nava",
+    img: aika,
+    title: "Founder",
+  },
+  {
+    name: "Reycel Sarmiento",
+    img: reycel,
+    title: "Founder",
+  },
+  {
+    name: "Els Dave Constantino",
+    img: els,
+    title: "Founder",
+  },
+  {
+    name: "Oliver Ladores",
+    img: ollie,
+    title: "Founder",
+  },
+];
 
 export default Carousel;
--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -10,59 +10,69 @@ const AboutUs = () => {
       <div className="w-auto h-[648px] flex justify-between items-center">
         {/* LAYER 1 */}
         <div className="flex flex-col items-center text-center mx-[80px] gap-2">
-        <img src= {w_blue} alt="" className="h-14 w-14"/> 
+          <img src={w_blue} alt="" className="h-14 w-14" />
           <h1
             className="font-poppins font-bold text-6xl mb-16 justify-center mx-[80px]"
-            style={{ color: "#7181A7" }}>
+            style={{ color: "#7181A7" }}
+          >
             About Us
           </h1>
-          <p className="text-start font-poppins text-xl mx-[80px]" style={{ color: "#7181A7" }}>
-            At Water-up, we’re revolutionizing the way you hydrate! 
-            Our vitamin-infused water is designed to give you a healthy boost in every sip, 
-            making it easier and more enjoyable to stay refreshed throughout the day.
-            With four invigorating flavors—classic, strawberry, peach, 
-            and lemon—there’s something for every taste, and we’re constantly 
-            innovating to bring you more. <br></br><br></br><br></br>
-
-            We believe hydration should be both healthy and delicious, 
-            which is why we only use natural flavors and essential vitamins to 
-            elevate your water experience. Whether you’re at the gym, on the 
-            go, or just need a flavorful pick-me-up, Water-up is your perfect 
-            hydration partner. Elevate your water, elevate your life!
+          <p
+            className="text-start font-poppins text-xl mx-[80px]"
+            style={{ color: "#7181A7" }}
+          >
+            At Water-up, we’re revolutionizing the way you hydrate! Our
+            vitamin-infused water is designed to give you a healthy boost in
+            every sip, making it easier and more enjoyable to stay refreshed
+            throughout the day. With four invigorating flavors—classic,
+            strawberry, peach, and lemon—there’s something for every taste, and
+            we’re constantly innovating to bring you more. <br></br>
+            <br></br>
+            <br></br>
+            We believe hydration should be both healthy and delicious, which is
+            why we only use natural flavors and essential vitamins to elevate
+            your water experience. Whether you’re at the gym, on the go, or just
+            need a flavorful pick-me-up, Water-up is your perfect hydration
+            partner. Elevate your water, elevate your life!
           </p>
         </div>
       </div>
 
       <div
         className="w-auto h-[500px] flex justify-center items-center"
-        style={{ backgroundColor: "#CFE4F4" }}>
+        style={{ backgroundColor: "#CFE4F4" }}
+      >
         {/* LAYER 2 */}
         <div className="flex flex-col items-center text-center mx-[80px]">
           {/* MISSION */}
           <h1
             className="font-poppins font-bold text-6xl mb-8"
-            style={{ color: "#7181A7" }}>
+            style={{ color: "#7181A7" }}
+          >
             Mission
           </h1>
           <p className="font-poppins text-xl" style={{ color: "#7181A7" }}>
-          To transform everyday hydration into a healthy, enjoyable experience by 
-          delivering vitamin-infused water packed with natural flavors. At Water-up, 
-          we aim to make staying hydrated effortless and delightful, helping individuals 
-          live healthier and more vibrant lives, one sip at a time.
+            To transform everyday hydration into a healthy, enjoyable experience
+            by delivering vitamin-infused water packed with natural flavors. At
+            Water-up, we aim to make staying hydrated effortless and delightful,
+            helping individuals live healthier and more vibrant lives, one sip
+            at a time.
           </p>
         </div>
         <div className="flex flex-col items-center text-center mx-[80px]">
           {/* VISION */}
           <h1
             className="font-poppins font-bold text-6xl mb-8"
-            style={{ color: "#7181A7" }}>
+            style={{ color: "#7181A7" }}
+          >
             Vision
           </h1>
           <p className="font-poppins text-xl" style={{ color: "#7181A7" }}>
-          To become the leading choice for health-conscious hydration, inspiring a 
-          global shift toward more nutritious and flavorful drinking options. We strive to 
-          innovate continuously, offering sustainable, eco-friendly products that elevate both
-          personal wellness and environmental responsibility.
+            To become the leading choice for health-conscious hydration,
+            inspiring a global shift toward more nutritious and flavorful
+            drinking options. We strive to innovate continuously, offering
+            sustainable, eco-friendly products that elevate both personal
+            wellness and environmental responsibility.
           </p>
         </div>
       </div>
