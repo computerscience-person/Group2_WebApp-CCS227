46ed4771ca7ea0d3dd6d15696e27d6cace94ee8c
hydraadra112 <johnmanuel.carado@wvsu.edu.ph>
2024-09-24 22:27:22 +0800

    Fixed not centered highlight card

--- src/components/HighlightCard.tsx
+++ src/components/HighlightCard.tsx
@@ -10,13 +10,9 @@ const HighlightCard: React.FC<HighlightCardProps> = ({
   contentText,
 }) => {
   return (
-    <div className="w-1/3 h-auto justify-start flex items-center transition-transform duration-80 ease-in-out hover:scale-105">
-      <img
-        src={imgSrc}
-        alt="Highlight Image"
-        className="w-auto h-[80px] pr-4"
-      />
-      <p className="font-poppins text-lg ml-4 " style={{ color: "#7181A7" }}>
+    <div className="w-80 h-auto justify-center flex items-center transition-transform duration-80 ease-in-out hover:scale-105">
+      <img src={imgSrc} alt="Highlight Image" className="w-auto h-24" />
+      <p className="font-poppins text-lg ml-3" style={{ color: "#7181A7" }}>
         {contentText}
       </p>
     </div>
